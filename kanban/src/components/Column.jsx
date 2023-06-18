4cd7f1a304a99115ed1ef4b4e021839ae2996eef
anuHait <anusmitahait67@gmail.com>
2023-06-19 01:13:32 +0530

    formatting

--- kanban/src/components/Column.jsx
+++ kanban/src/components/Column.jsx
@@ -9,9 +9,9 @@ const Column = ({ title, color, tasks = [], droppableId }) => {
     <div className="w-full md:w-[45%] p-8 -mt-10">
       <div className="flex gap-3 justify-start items-center bg-[#F5F5F5] rounded-md p-3">
         <div className={`w-2 h-2 p-1 rounded-full ${color} mb-3`}></div>
-        <h2 className="text-2xl font-semibold mb-4">{title}</h2>
+        <h2 className="text-xl font-semibold mb-4">{title}</h2>
         <div className="bg-gray-300 w-3 h-3 rounded-full p-3 flex justify-center items-center -mt-3">
-          <span className="text-gray-600">{cardCount}</span>
+          <span className="text-gray-600 text-sm">{cardCount}</span>
         </div>
       </div>
 
