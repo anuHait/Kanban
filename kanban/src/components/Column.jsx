a710c86a7a867160106cb677b1bd309a391ceacf
anuHait <anusmitahait67@gmail.com>
2023-06-19 01:15:01 +0530

    corner change

--- kanban/src/components/Column.jsx
+++ kanban/src/components/Column.jsx
@@ -7,7 +7,7 @@ const Column = ({ title, color, tasks = [], droppableId }) => {
 
   return (
     <div className="w-full md:w-[45%] p-8 -mt-10">
-      <div className="flex gap-3 justify-start items-center bg-[#F5F5F5] rounded-md p-3">
+      <div className="flex gap-3 justify-start items-center bg-[#F5F5F5] rounded-lg p-3">
         <div className={`w-2 h-2 p-1 rounded-full ${color} mb-3`}></div>
         <h2 className="text-xl font-semibold mb-4">{title}</h2>
         <div className="bg-gray-300 w-3 h-3 rounded-full p-3 flex justify-center items-center -mt-3">
